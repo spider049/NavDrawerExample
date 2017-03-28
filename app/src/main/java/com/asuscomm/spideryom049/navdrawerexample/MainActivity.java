ab34eebd5e940e8a5e3b48e4b3580aee52f3ae88
spideryom <spideryom049@gmail.com>
2017-03-29 00:57:13 +0700

    ข้อความ

--- app/src/main/java/com/asuscomm/spideryom049/navdrawerexample/MainActivity.java
+++ app/src/main/java/com/asuscomm/spideryom049/navdrawerexample/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+    //66
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
